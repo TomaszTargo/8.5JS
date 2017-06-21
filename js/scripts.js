4a778030c2f9f2055af6643892daf6868e9a18f0
tomasz <tomasztargo@gmail.com>
2017-06-21 13:02:44 +0200

    przypisanie zmiennych

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+//zmienne
+var a = 4,
+	h = 5,
+	triangleArea = a*h/2;
+
+// informacja w konsoli
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
