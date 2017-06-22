fd6912ccd0953f6bcab249113ca67cd9c3b352b8
tomasz <tomasztargo@gmail.com>
2017-06-22 08:32:42 +0200

    poprawione wcięcia

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // scripts.js
 //zmienne
 var a = 4,
-	h = 5,
-	triangleArea = a*h/2;
+		h = 5,
+		triangleArea = a*h/2;
 
 // informacja w konsoli
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
